4b1e45a7512d39ca74999b302a334476418e7a7f
William Koller <wkoller25@gmail.com>
2020-06-15 13:55:01 -0300

    feat: add config database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: 'localhost',
+  dialect: 'mysql',
+  username: 'admin',
+  password: 'developer30',
+  database: 'express_sequelize',
+  port: 5050,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+}
