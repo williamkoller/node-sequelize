6a940762c346846baaccbe4a1c92ad729ee14d88
William Koller <wkoller25@gmail.com>
2020-06-15 19:24:29 -0300

    feat: add require database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const routers = require('./routes/routes')
+require('./database')
 const app = express()
+const port = 3333
 
 app.use(express.json())
 app.use(routers)
 
-app.listen(3333)
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
