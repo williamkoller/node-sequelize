167c662599b482c143b8002fdae80701c81620e9
William Koller <wkoller25@gmail.com>
2020-06-17 01:36:02 -0300

    feat: add auth in router address

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
+const AddressController = require('../controllers/AddressController')
 const authMiddleware = require('../middleware/auth')
 const router = express.Router()
 
 router.get('/users', authMiddleware, UserController.index)
-
 router.post('/users', UserController.store)
-
 router.put('/users/:user_id', UserController.update)
-
 router.delete('/users/:user_id', UserController.delete)
-
 router.post('/users/login', UserController.login)
+
+router.use(authMiddleware)
+
+router.get('/users/:user_id/address', AddressController.index)
+router.post('/users/:user_id/address', AddressController.store)
+router.delete('/users/:id/address', AddressController.delete)
+router.put('/users/:id/address', AddressController.update)
 
 module.exports = router
